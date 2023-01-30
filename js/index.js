e84a8cf2d9f098cb52238abefc50c1289fa5233c
1kSunnyGO <110897975+1kSunnyGO@users.noreply.github.com>
2023-01-30 02:01:41 -0500

    updates

---
+++ js/index.js
@@ -0,0 +1,39 @@
+console.log(
+    "SUCCESS"
+)
+
+
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.workImg', {
+    duration: 700,
+    origin: "bottom",
+    distance: "75px",
+    easing: "ease-in-out",
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
